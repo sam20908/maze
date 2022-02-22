0f97f65e967542274b65cac1fc9ee6aac2e1c6b2
Sam Huang <samestimable2016@gmail.com>
2022-02-21 21:52:22 -0800

    Print solution in colors

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
+use colored::*;
 use rand::distributions::{Bernoulli, Distribution};
 use rand::{thread_rng, Rng};
-use std::collections::VecDeque;
 
-const WIDTH: usize = 50usize; // at least 3
-const HEIGHT: usize = 45usize; // at least 3
+const WIDTH: usize = 40usize; // at least 3
+const HEIGHT: usize = 40usize; // at least 3
 
-type MazeArray = [[bool; WIDTH]; HEIGHT];
+type MazeArray = [[char; WIDTH]; HEIGHT];
 type Position = (usize, usize);
-type Path = VecDeque<Position>;
+type Path = Vec<Position>;
 
 fn print_maze(maze: &MazeArray) {
     for row in maze.iter() {
-        print!("[ ");
-
-        for col in row {
-            print!("{} ", *col as i32);
+        for col in row.iter() {
+            if *col == '+' {
+                print!("{} ", "+".yellow());
+            } else {
+                print!("{} ", *col);
+            }
         }
-
-        println!("]");
+        println!("");
     }
 }
 
 fn gen_maze() -> (MazeArray, Position, Position) {
-    let mut maze = [[false; WIDTH]; HEIGHT]; // false indicates wall, true indicates clear path
+    let mut maze = [['#'; WIDTH]; HEIGHT]; // # indicates wall, . indicates clear path
 
     let start = thread_rng().gen_range(1, HEIGHT - 1);
     let end = thread_rng().gen_range(1, HEIGHT - 1);
-    maze[start][0] = true;
-    maze[end][WIDTH - 1] = true;
+    maze[start][0] = '.';
+    maze[end][WIDTH - 1] = '.';
 
     let dist = Bernoulli::new(0.5).expect("Bernoulli distribution failed!");
     for i in 1..HEIGHT - 1 {
         for j in 1..WIDTH - 1 {
-            maze[i][j] = dist.sample(&mut rand::thread_rng());
+            if dist.sample(&mut rand::thread_rng()) {
+                maze[i][j] = '.';
+            }
         }
     }
 
@@ -41,36 +44,36 @@ fn gen_maze() -> (MazeArray, Position, Position) {
 
 fn dfs(
     maze: &MazeArray,
-    vis: &mut MazeArray,
+    vis: &mut [[bool; WIDTH]; HEIGHT],
     pos: Position,
     end: Position,
     path: &mut Path,
 ) -> bool {
     if pos == end {
-        path.push_front(pos);
+        path.push(pos);
         return true;
     }
-    if !maze[pos.0][pos.1] {
+    if maze[pos.0][pos.1] == '#' {
         return false;
     }
     vis[pos.0][pos.1] = true;
 
     if pos.1 > 0 {
         if !vis[pos.0][pos.1 - 1] && dfs(maze, vis, (pos.0, pos.1 - 1), end, path) {
-            path.push_front(pos);
+            path.push(pos);
             return true;
         }
     }
     if !vis[pos.0][pos.1 + 1] && dfs(maze, vis, (pos.0, pos.1 + 1), end, path) {
-        path.push_front(pos);
+        path.push(pos);
         return true;
     }
     if !vis[pos.0 - 1][pos.1] && dfs(maze, vis, (pos.0 - 1, pos.1), end, path) {
-        path.push_front(pos);
+        path.push(pos);
         return true;
     }
     if !vis[pos.0 + 1][pos.1] && dfs(maze, vis, (pos.0 + 1, pos.1), end, path) {
-        path.push_front(pos);
+        path.push(pos);
         return true;
     }
 
@@ -89,7 +92,7 @@ fn solve(maze: &MazeArray, start: Position, end: Position) -> Option<Path> {
 }
 
 fn main() {
-    let mut maze = [[false; WIDTH]; HEIGHT];
+    let mut maze = [['#'; WIDTH]; HEIGHT];
     let mut path: Path = Path::default();
     let mut found = false;
     while !found {
@@ -102,6 +105,8 @@ fn main() {
         }
     }
 
+    for (x, y) in path {
+        maze[x][y] = '+';
+    }
     print_maze(&maze);
-    println!("Path is {:?}", path);
 }
